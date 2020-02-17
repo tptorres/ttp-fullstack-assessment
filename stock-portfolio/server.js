1d05dfa8e569e2aced52cb1fbcc9ae4cf76ff3a9
Tyler <tptorres5@gmail.com>
2020-02-17 16:31:49 -0500

    Deployment issues

--- stock-portfolio/server.js
+++ stock-portfolio/server.js
@@ -1,17 +1,14 @@
 const express = require('express');
-const app = express();
 const connectDB = require('./config/database');
 const path = require('path');
 
+const app = express();
+
 // Connect to MongoDB
 connectDB();
+
 // Middleware
-
 app.use(express.json({ extended: false })); // can accept body data now
-
-// Environment variable for production, but 7000 for development
-const PORT = process.env.PORT || 7000;
-app.listen(PORT, () => console.log(`Server started on ${PORT}`));
 
 // Defining routes to be used
 app.use('/api/users', require('./routes/users'));
@@ -22,9 +19,12 @@ app.use('/api/transactions', require('./routes/transactions'));
 // Serve static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-
   // We are loading the static index.html file signifying production
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
+
+// Environment variable for production, but 7000 for development
+const PORT = process.env.PORT || 7000;
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
